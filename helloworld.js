57117d5fecd6512739df32a3ed5d38b71e46a343
sebiwnl <69086256+sebiwnl@users.noreply.github.com>
2020-10-30 21:49:27 +0100

    Checks if given letter is consonant or vowel

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,10 @@
-console.log(Hello World!');
-alert('hellor world');
+<html>
+ <head>
+   <title> My title </title>
+     </head>
+     <body>
+         </body>
+</html>
+<script>
+document.write(<h1> " Hello World" </h1>)
+</script>
